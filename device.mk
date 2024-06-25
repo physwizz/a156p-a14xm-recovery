9cd3d3968e6d28236995a4a8e6100b0818ccc766
ramenrolls <ramenrolls@proton.me>
2024-06-25 13:53:00 +0200

    a14xm: inherit aosp base configuration

--- device.mk
+++ device.mk
@@ -6,3 +6,7 @@
 #
 
 LOCAL_PATH := device/samsung/a14xm
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
